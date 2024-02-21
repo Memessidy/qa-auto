e1d4ffaeacf02e72bac24381c2715b8bd82dfc46
YehorMukomel <mukomelegor@gmail.com>
2024-02-21 20:26:22 +0200

    api tests changed

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -41,16 +41,27 @@ def test_emoji_exists(github_api):
 
 
 @pytest.mark.api
-def test_emojis_count_more_than_one_thousand(github_api):
-    emojis_length = len(github_api.get_emojis())
-    assert emojis_length > 1000
-
-
-@pytest.mark.api
-def test_first_commit_exists(github_api):
-    owner = 'Memessidy'
-    repo = 'qa-auto'
+@pytest.mark.parametrize("owner,repo", [('Memessidy', 'qa-auto')])
+def test_first_commit_exists(github_api, owner, repo):
     r = github_api.get_commits(owner, repo)
     assert r[-1]['commit']['author']['name'] == 'YehorMukomel'
     assert r[-1]['commit']['author']['email'] == 'mukomelegor@gmail.com'
     assert r[-1]['commit']['message'] == 'initial'
+
+
+@pytest.mark.api
+@pytest.mark.parametrize(
+    "owner,repo,sha,expected_message", [('Memessidy', 'qa-auto', '27deed1a658b4a2d7e719745d83789c4e7251b7f',
+                                         'rozetka tests has been added')])
+def test_check_message_in_commit(github_api, owner, repo, sha, expected_message):
+    msg = github_api.get_commit_by_sha(owner, repo, sha)['commit']['message']
+    assert expected_message in msg
+
+
+@pytest.mark.api
+@pytest.mark.parametrize("owner,repo,sha", [('Memessidy', 'qa-auto', '43cac8bd7295612eab2c1fb534cf733d4327cf3e')])
+def test_check_author_is_committer(github_api, owner, repo, sha):
+    response = github_api.get_commit_by_sha(owner, repo, sha)
+    committer = response['commit']['author']['name']
+    author = response['commit']['committer']['name']
+    assert committer == author
