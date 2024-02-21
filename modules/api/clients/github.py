8cf06661ba1446306367beabdca0d133936c433f
YehorMukomel <mukomelegor@gmail.com>
2024-02-21 20:26:22 +0200

    api tests changed

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -22,3 +22,15 @@ class Github:
     def get_commits(self, owner, repo):
         r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits')
         return r.json()
+
+    def get_commit_by_sha(self, owner, repo, sha):
+        r = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits/{sha}")
+        return r.json()
+
+
+if __name__ == '__main__':
+    g = Github()
+    owner = 'Memessidy'
+    repo = 'qa-auto'
+    hash = '4071929a6f78368887fc30eda716c3d7f60d68a5'
+    print(g.get_commit_by_sha(owner, repo, hash))
